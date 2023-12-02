637809f6348b8806a9707e54494c2f8565873437
luciadobre <lcdobre@yahoo.com>
2023-12-02 15:58:36 +0200

    add users

--- migrate.php
+++ migrate.php
@@ -31,7 +31,19 @@ if ($conn->connect_error) {
 
 // crearea bazei de date cu tabel
 $sql = "
-    CREATE TABLE IF NOT EXISTS articole (
+    DROP TABLE IF EXISTS utilizatori;
+    DROP TABLE IF EXISTS articole;
+    
+    CREATE TABLE utilizatori (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nume VARCHAR(255) NOT NULL,
+        user VARCHAR(255) NOT NULL,
+        parola VARCHAR(255) NOT NULL,
+        rol VARCHAR(50) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    
+    CREATE TABLE articole (
         id INT AUTO_INCREMENT PRIMARY KEY,
         titlu VARCHAR(255) NOT NULL,
         autor VARCHAR(255) NOT NULL,
@@ -39,12 +51,16 @@ $sql = "
         categorie VARCHAR(50) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
-
-    INSERT INTO articole (title, author, content, category) VALUES
-    ('articol 1', 'autor 1', 'continut 1', 'artistic'),
-    ('articol 2', 'autor 2', 'continut 2', 'tehnic'),
-    ('articol 3', 'autor 3', 'continut 3', 'stiinta'),
-    ('articol 4', 'autor 4', 'continut 4', 'moda');
+    
+    INSERT INTO utilizatori (nume, user, parola, rol) VALUES
+    ('Jurnalist 1', 'jurnalist1', 'password1', 'jurnalist'),
+    ('Editor 1', 'editor1', 'password1', 'editor'),
+    ('Cititor 1', 'cititor1', 'password1', 'cititor');
+    
+    INSERT INTO articole (titlu, autor, continut, categorie) VALUES
+    ('Articol 1', 'Jurnalist 1', 'Continut articol 1', 'artistic'),
+    ('Articol 2', 'Jurnalist 1', 'Continut articol 2', 'tehnic'),
+    ('Articol 3', 'Jurnalist 1', 'Continut articol 3', 'stiinta');
 ";
 
 if ($conn->multi_query($sql) === TRUE) {
